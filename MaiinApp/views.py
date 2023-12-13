8117aab1e8f9ac2800342c385f98cfc5b9103263
TatiannnaF <tatiana.funny@gmail.com>
2023-12-13 12:34:17 +0300

    Выполнено задание по Модулю-1 полностью

--- MaiinApp/views.py
+++ MaiinApp/views.py
@@ -39,6 +39,7 @@ def get_item(request, id):
             result = f"""
             <h2>Имя: {item["name"]} </h2>
             <p>Количество: {item['quantity']} </p>
+            <a href='/items'> Назад </a>
             """
             return HttpResponse(result)
     return HttpResponseNotFound(f'Item with id={id} not found')
@@ -46,7 +47,7 @@ def get_item(request, id):
 def items_list(request):
     result = "<h2>Список товаров</h2><ol>"
     for item in items:
-        result += f"<li>{item['name']}</li>"
+        result += f"<li><a href='/item/{item['id']}'>{item['name']}</a></li>"
     result += '</ol>'
     return HttpResponse(result)
 
