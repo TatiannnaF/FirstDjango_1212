b4d6855b88fb97def5116039c6536dd36ea60cd7
TatiannnaF <tatiana.funny@gmail.com>
2023-12-14 11:41:17 +0300

    Добавлена работа с базой Item models

--- FirstDjango/settings.py
+++ FirstDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MaiinApp',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
