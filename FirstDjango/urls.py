4f16481cb29034ba47e01c672e840740d8acfb07
TatiannnaF <tatiana.funny@gmail.com>
2023-12-15 11:31:34 +0300

    Исправили ошибки и добавили description

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from MaiinApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about', views.about),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
     path('item/<int:id>', views.get_item, name='item-detail'),
     path('items', views.items_list, name='items-list')
 ]
--- MaiinApp/models.py
+++ MaiinApp/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, default="Какое-то описание")
 
     def __repr__(self):
         return f"Item{self.name, self.brand, self.count}"
